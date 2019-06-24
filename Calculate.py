13ca7e6d53a23773958ed95089ba2adf023e75ed
3194libin <15764747737@qq.com>
2019-06-24 09:00:03 +0800

    Initial commit

---
+++ Calculate.py
@@ -0,0 +1,3 @@
+import TemperatureConversion
+print("32摄氏度 = %.2f 华氏度" % TemperatureConversion.ctof(32))
+print("99华氏度 = %.2f 摄食度" % TemperatureConversion.ftoc(99))
---
+++ TemperatureConversion.py
@@ -0,0 +1,6 @@
+def ftoc(f):
+    c = (f-32)/1.8
+    return c
+def ctof(c):
+    f = c*1.8+32
+    return f
